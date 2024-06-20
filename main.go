6135f5e239721695432b84b4b8d24ccc271e692f
FLOpeSS <sneto4413@gmail.com>
2024-06-20 18:08:20 -0300

    Removing func saveHTML

--- main.go
+++ main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"fmt"
 	"gomark/handlers"
-	"html/template"
 	"net/http"
 )
 
 const SERVERPORT = ":8000"
-
-func safeHTML(s string) template.HTML {
-	return template.HTML(s)
-}
 
 func main() {
 	http.HandleFunc("/", handlers.HomePageHandler)
